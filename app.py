29aa430d3a44730daa97f1db1d65e9cb6d8225d0
dustinmarshall <dustinmarshall@uchicago.edu>
2023-03-04 16:21:52 -0600

    updated app to use pinecone

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template, request
 import openai
-import pandas as pd
-import json
 import time
-import numpy as np
-import tiktoken
-import os
+import pinecone
 
 app = Flask(__name__)
 
 # set up the API key
-openai.api_key = "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"
-
-# import pandas dataframe from jsonl file
-df = pd.read_csv(os.path.join(os.path.dirname(__file__), "embeddings/medical_dialogues_cleaned.csv"))
-
-# import json file as dict
-with open(os.path.join(os.path.dirname(__file__), "embeddings/text_embeddings.json")) as json_file:
-    document_embeddings = json.load(json_file)  
+openai.api_key = "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"
 
 def get_embedding(text: str, model: str="text-embedding-ada-002") -> list[float]:
     retry_count = 0
@@ -35,54 +24,24 @@ def get_embedding(text: str, model: str="text-embedding-ada-002") -> list[float]
             print("Caught exception, retrying in 5 seconds...")
             time.sleep(5)
 
-def vector_similarity(x: list[float], y: list[float]) -> float:
-    """
-    Returns the similarity between two vectors.
-    
-    Because OpenAI Embeddings are normalized to length 1, the cosine similarity is the same as the dot product.
-    """
-    return np.dot(np.array(x), np.array(y))
-
-def order_document_sections_by_query_similarity(query: str, contexts: dict[(str), np.array]) -> list[(float, (str, str))]:
-    """
-    Find the query embedding for the supplied query, and compare it against all of the pre-calculated document embeddings
-    to find the most relevant sections. 
-    
-    Return the list of document sections, sorted by relevance in descending order.
-    """
-    query_embedding = get_embedding(query)
-    
-    document_similarities = sorted([
-        (vector_similarity(query_embedding, doc_embedding), doc_index) for doc_index, doc_embedding in contexts.items()
-    ], reverse=True)
-    
-    return document_similarities
-
-def construct_prompt(question: str, context_embeddings: dict, df: pd.DataFrame) -> str:
+def construct_prompt(query: str) -> str:
     """
     Fetch relevant 
     """
-    most_relevant_document_sections = order_document_sections_by_query_similarity(question, context_embeddings)
+    pinecone.init(api_key="e5a609b1-133e-4c44-8629-51c9bb3a6104", environment="us-east1-gcp")
+    index = pinecone.Index("medical-dialog-embeddings")
+    query_embedding = get_embedding(query,model="text-embedding-ada-002")
+    most_relevant_answers = index.query([query_embedding], top_k=5, include_metadata=True)
+
+    answers_to_insert = []
+    for i in most_relevant_answers["matches"]:
+        answers_to_insert.append("\n- " + i["metadata"]["answer_raw"])
     
-    chosen_sections = []
-    chosen_sections_len = 0
-    chosen_sections_indexes = []
-     
-    for _, section_index in most_relevant_document_sections:
-        # Add contexts until we run out of space.        
-        document_section = df.loc[int(section_index)]
-        
-        chosen_sections_len += document_section.tokens + len(tiktoken.get_encoding("gpt2").encode("\n* "))
-        if chosen_sections_len > 500:
-            break
-            
-        chosen_sections.append("\n* " + document_section.answer)
-        chosen_sections_indexes.append(str(section_index))
+    header = """\n\nIf the question above is not about pregnancy or the answer is not contained within the 
+    text below, say "I'm sorry, that isn't within my expertise." Otherwise, answer the question above 
+    as truthfully as possible using the context below:\n\n"""
     
-    header = """Answer the question as truthfully as possible using the provided context, and if the answer 
-    is not contained within the text below, say "I'm sorry, that isn't within my expertise."\n\nContext:\n"""
-    
-    return header + "".join(chosen_sections) + "\n\n Q: " + question + "\n A:"
+    return "Q: " + query + header + "".join(answers_to_insert) + "\n\nA: "
 
 COMPLETIONS_API_PARAMS = {
     "temperature": 0.0,
@@ -90,17 +49,9 @@ COMPLETIONS_API_PARAMS = {
     "model": "text-davinci-003",
 }
 
-def answer_query_with_context(
-    query: str,
-    df: pd.DataFrame,
-    document_embeddings: dict[str, np.array],
-    show_prompt: bool = False
-) -> str:
-    prompt = construct_prompt(
-        query,
-        document_embeddings,
-        df
-    )
+def answer_query_with_context(query: str, show_prompt: bool = False) -> str:
+    
+    prompt = construct_prompt(query)
     
     if show_prompt:
         print(prompt)
@@ -118,7 +69,7 @@ def home():
         # Get the text from the input form
         text = request.form['text']
         # Call the reverse_text function on the input text
-        answer = answer_query_with_context(text, df, document_embeddings)
+        answer = answer_query_with_context(text)
         # Return the reversed text to the user
         return render_template('index.html', answer=answer, text=text)
     else:
--- embeddings/compute_text_embeddings.py
+++ embeddings/compute_text_embeddings.py
@@ -3,12 +3,15 @@ import pandas as pd
 import time
 import json
 import os
+import pinecone
+from tqdm.auto import tqdm
 
 # set up the API key
-openai.api_key = "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"
+openai.api_key = "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"
 
-# import pandas dataframe from jsonl file
-df = pd.read_csv(os.path.join(os.path.dirname(__file__), "embeddings/medical_dialogues_cleaned.csv"))
+# import json as dict
+with open(os.path.join(os.path.dirname(__file__), "medical_dialogues_cleaned.json")) as json_file:
+    data = json.load(json_file)
 
 def get_embedding(text: str, model: str="text-embedding-ada-002") -> list[float]:
     retry_count = 0
@@ -18,32 +21,44 @@ def get_embedding(text: str, model: str="text-embedding-ada-002") -> list[float]
               model=model,
               input=text
             )
-            return result["data"][0]["embedding"]
+            return result
         except Exception as e:
+            print(e)
             retry_count += 1
             if retry_count == 3:
-                print(e)
-            print("Caught exception, retrying in 5 seconds...")
-            time.sleep(5)
+                print("Skipping batch with error:", e)
+            print("Caught exception, retrying in 15 seconds...")
+            time.sleep(15)
 
-def compute_doc_embeddings(df: pd.DataFrame) -> dict[str, list[float]]:
-    """
-    Create an embedding for each row in the dataframe using the OpenAI Embeddings API.
-    
-    Return a dictionary that maps between each embedding vector and the index of the row that it corresponds to.
-    """
-    embeddings = {}
-    count = 0
-    for idx, r in df.iterrows():
-        embeddings[idx] = get_embedding(r.description_question)
-        count += 1
-        if count % 500 == 0:
-            print(count, "rows processed")
-    return embeddings
+# set index name
+index_name = 'medical-dialog-embeddings'
 
-# compute document embeddings
-document_embeddings = compute_doc_embeddings(df)
+# initialize connection to pinecone (get API key at app.pinecone.io)
+pinecone.init(
+    api_key="e5a609b1-133e-4c44-8629-51c9bb3a6104",
+    environment="us-east1-gcp"
+)
+# check if 'openai' index already exists (only create index if not)
+if index_name not in pinecone.list_indexes():
+    pinecone.create_index(index_name, dimension=len(1536))
+# connect to index
+index = pinecone.Index(index_name)
 
-# export dict to json
-with open('text_embeddings.json', 'w') as fp:
-    json.dump(document_embeddings, fp)
+# convert to pandas dataframe
+df = pd.DataFrame(data)
+count = 0  # we'll use the count to create unique IDs
+batch_size = 50  # process everything in batches of 50
+for i in tqdm(range(0, len(df), batch_size)):
+    # set end position of batch
+    i_end = min(i+batch_size, len(df))
+    # get batch of lines and IDs
+    rows_batch = df.iloc[i:i+batch_size]
+    ids_batch = [str(n) for n in range(i, i_end)]
+    # create embeddings
+    res = get_embedding(rows_batch["Description"].tolist())
+    embeds = [record['embedding'] for record in res['data']]
+    # prep metadata and upsert batch
+    meta = [{'question_summary': row['Description'], 'question_raw': row['Patient'], 'answer_raw': row['Doctor']} for _, row in rows_batch.iterrows()]
+    to_upsert = zip(ids_batch, embeds, meta)
+    # upsert to Pinecone
+    index.upsert(vectors=list(to_upsert))
